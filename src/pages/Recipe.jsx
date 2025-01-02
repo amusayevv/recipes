85452ab5ed6ed3636b4710b13116a1e7e46bbbf7
Alisa Musayev <amusayev20487@ada.edu.az>
2025-01-02 10:40:51 +0400

    added search functionality and working popup

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,31 +1,92 @@
-import React, {useEffect, useState} from "react"
-import RecipeCard from '../components/RecipeCard';
+import React, { useEffect, useState } from "react";
+import RecipeCard from "../components/RecipeCard";
+import Search from "../components/Search";
 
-
-function Recipe () {
-    const [recipe, setRecipe] = useState(null);
+function Recipe() {
+    const [allRecipes, setAllRecipes] = useState([]);
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+    const [selectedRecipe, setSelectedRecipe] = useState(null);
 
     useEffect(() => {
-      fetch("http://localhost:3000/recepies")
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setRecipe(data);
-          console.log(data);
-        })
-    }, [])
-  
+        fetch("http://localhost:3000/recepies")
+            .then((res) => res.json())
+            .then((data) => {
+                setAllRecipes(data);
+                setFilteredRecipes(data);
+            })
+            .catch((err) => console.error("Error fetching recipes:", err));
+    }, []);
+
+    function handleClick(id) {
+        const currentRecipe = allRecipes.find((item) => item.id === id);
+        setSelectedRecipe(currentRecipe);
+    }
+
+    function closePopup() {
+        setSelectedRecipe(null);
+    }
+
+    function handleSearch(event) {
+        const searchText = event.target.value.toLowerCase();
+        const filtered = allRecipes.filter((item) =>
+            item.title.toLowerCase().includes(searchText)
+        );
+        setFilteredRecipes(filtered);
+    }
+
     return (
         <div>
+            <Search handleSearch={handleSearch} />
             <div className="cards-flex">
-                {recipe && recipe.map(item => {
-                    return <RecipeCard key={item.id} id={item.id} title={item.title} description={item.description} difficulty_level={item.difficulty_level} tags={item.tags} />
-                })}
+                {filteredRecipes.map((item) => (
+                    <RecipeCard
+                        key={item.id}
+                        id={item.id}
+                        title={item.title}
+                        description={item.description}
+                        difficulty_level={item.difficulty_level}
+                        tags={item.tags}
+                        openPopup={() => handleClick(item.id)}
+                    />
+                ))}
             </div>
+
+            {selectedRecipe && (
+                <div className="recipe-popup">
+                    <div onClick={closePopup} className="popup-bg"></div>
+                    <RecipeCard
+                        key={selectedRecipe.id}
+                        id={selectedRecipe.id}
+                        title={selectedRecipe.title}
+                        description={selectedRecipe.description}
+                        difficulty_level={selectedRecipe.difficulty_level}
+                        tags={selectedRecipe.tags}
+                        openPopup={() => handleClick(selectedRecipe.id)}
+                    >
+                        <div className="middle-flex">
+                            <ol>
+                                <p>
+                                    {selectedRecipe.preparation_steps.map(
+                                        (item) => {
+                                            return <li>{item}</li>;
+                                        }
+                                    )}
+                                </p>
+                            </ol>
+
+                            <ul>
+                                <p>
+                                    {selectedRecipe.ingredients.map((item) => {
+                                        return <li>{item}</li>;
+                                    })}
+                                </p>
+                            </ul>
+                        </div>
+                    </RecipeCard>
+                </div>
+            )}
         </div>
-    
-    )
+    );
 }
 
 export default Recipe;
