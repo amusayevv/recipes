bb8729e028b666c1ec3893be17f1b1fe84af7ac7
guneljavadova <gjavadova16098@ada.edu.az>
2025-01-02 20:46:10 +0400

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Home () {
-    return <h1>Home Page</h1>
+    return( 
+        <div>
+            <section >
+                <h2>Welcome to the Recipe Manager App!</h2>
+                <p>The Best App for Recipes You Want Try Today. </p>
+            </section>
+
+            <br /><br/>
+
+            <section>
+                <h2>Featured Recipe</h2>
+                <div classname="featured">
+                    <h3>name of recipe</h3>
+                    <p>ingredients</p>
+                    {/* <a href="#" target="_blank">View Full Recipe</a> */}
+                </div>
+            </section>
+            <br /><br />
+            <section>
+                <h2>Projects</h2>
+                
+                <p>We had two projects during this Web & Mobile 1 class. This app is one these projects. 
+                    Another one was about creating extension:   
+                         <a href="https://github.com/amusayevv/auto-form-filler.git" target="_blank"> GitHub repository link. </a>
+                </p>
+            </section>
+            
+        </div>
+
+    );
 }
 
 export default Home;
