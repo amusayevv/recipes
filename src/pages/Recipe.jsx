d88996ee33d93f2cd4d6e60424c2d58fc84c7c1e
Alisa Musayev <amusayev20487@ada.edu.az>
2025-01-04 15:41:11 +0400

    adding functionality part 1

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -2,13 +2,61 @@ import React, { useEffect, useState } from "react";
 import RecipeCard from "../components/RecipeCard";
 import Search from "../components/Search";
 
+function AddPopup() {
+    return (
+        <form className="add" action="post">
+            <label htmlFor="add-title">Title</label>
+            <input
+                id="add-title"
+                type="text"
+                className="add-title"
+                placeholder="Title"
+            />
+            <label htmlFor="add-description">Description</label>
+            <input
+                id="add-description"
+                type="text"
+                className="add-description"
+                placeholder="Description"
+            />
+            <label htmlFor="add-ingredients">Ingredients</label>
+            <input
+                id="add-ingredients"
+                type="text"
+                className="add-popup-ingredients"
+                placeholder="add ingredients separated by comma"
+            />
+            <label htmlFor="add-ingredients">Difficulty</label>
+            <select name="cars" id="add-difficulty">
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+            </select>
+            <label htmlFor="add-preparation">Preparation steps</label>
+            <input
+                id="add-preparation"
+                type="text"
+                className="add-popup-ingredients"
+                placeholder="describe preparation steps separated by comma"
+            />
+            <label htmlFor="add-tags">Tags</label>
+            <input
+                id="add-tags"
+                type="text"
+                className="add-popup-ingredients"
+                placeholder="add tags separated by comma"
+            />
+        </form>
+    );
+}
+
 function Recipe() {
     const [allRecipes, setAllRecipes] = useState([]);
     const [filteredRecipes, setFilteredRecipes] = useState([]);
     const [selectedRecipe, setSelectedRecipe] = useState(null);
 
     useEffect(() => {
-        fetch("http://localhost:3000/recepies")
+        fetch("http://localhost:3000/recipes")
             .then((res) => res.json())
             .then((data) => {
                 setAllRecipes(data);
@@ -28,15 +76,25 @@ function Recipe() {
 
     function handleSearch(event) {
         const searchText = event.target.value.toLowerCase();
-        const filtered = allRecipes.filter((item) =>
-            item.title.toLowerCase().includes(searchText)
-        );
+        const filtered = allRecipes.filter((item) => {
+            const titleMatch = item.title.toLowerCase().includes(searchText);
+            const descriptionMatch = item.description
+                .toLowerCase()
+                .includes(searchText);
+            const tagsMatch = item.tags.some((tag) =>
+                tag.toLowerCase().includes(searchText)
+            );
+            return titleMatch || descriptionMatch || tagsMatch;
+        });
         setFilteredRecipes(filtered);
     }
 
     return (
         <div>
-            <Search handleSearch={handleSearch} />
+            <div className="search-flex">
+                <Search handleSearch={handleSearch} />
+                <button className="add-button primary">Add</button>
+            </div>
             <div className="cards-flex">
                 {filteredRecipes.map((item) => (
                     <RecipeCard
@@ -85,6 +143,8 @@ function Recipe() {
                     </RecipeCard>
                 </div>
             )}
+
+            <AddPopup />
         </div>
     );
 }
